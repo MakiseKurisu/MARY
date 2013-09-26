be05f3868092a5d44c9e87f8f200d131ed3787d4
MakiseKurisu <saberconer@gmail.com>
2013-09-25 22:29:30 -0500

    Working on the syntax highlighting
    Based on the Iczelion's Win32 Assembly Tutorial part 35

---
+++ Editor/Resource.h
@@ -0,0 +1,50 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC                      -1
+#endif
+
+#pragma region Menu
+#define IDR_MAINMENU                    100
+#define IDM_OPEN                        1000
+#define IDM_SAVE                        1001
+#define IDM_CLOSE                       1002
+#define IDM_SAVEAS                      1003
+#define IDM_EXIT                        1004
+
+#define IDM_UNDO                        1010
+#define IDM_REDO                        1011
+#define IDM_COPY                        1012
+#define IDM_CUT                         1013
+#define IDM_PASTE                       1014
+#define IDM_DELETE                      1015
+#define IDM_SELECTALL                   1016
+
+#define IDM_FIND                        1020
+#define IDM_FINDNEXT                    1021
+#define IDM_FINDPREV                    1022
+#define IDM_REPLACE                     1023
+#define IDM_GOTOLINE                    1024
+
+#define IDM_OPTION                      1030
+#pragma endregion Menu
+
+#pragma endregion Dialog
+#define IDD_OPTIONDLG                   200
+#define IDC_BACKCOLORBOX                1100
+#define IDC_TEXTCOLORBOX                1101
+
+#define IDD_FINDDLG                     201
+#define IDC_FINDEDIT                    1200
+#define IDC_MATCHCASE                   1201
+#define IDC_WHOLEWORD                   1202
+#define IDC_DOWN                        1203
+#define IDC_UP                          1204
+
+#define IDD_GOTODLG                     202
+#define IDC_LINENO                      1300
+
+#define IDD_REPLACEDLG                  203
+#define IDC_REPLACEEDIT                 1401
+#pragma endregion Dialog
+
+#define IDR_MAINACCEL                   300
+
